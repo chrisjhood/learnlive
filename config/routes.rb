1167e31ed43f814244d69b95fe2e6c329e047035
Jonwheeler <jon.wheeler7@gmail.com>
2012-08-19 09:03:58 -0500

    t

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 LearnLive::Application.routes.draw do
-  root :to => 'pages#login'
+  root :to => 'pages#home'
+  match '/auth/facebook/callback', to: 'sessions#create', :as => :login
+  match '/auth/failure', to: 'pages#home'
+  match '/logout', to: 'sessions#destroy', :as => :logout
 end
