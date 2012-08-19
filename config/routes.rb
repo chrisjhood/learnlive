f8e79cf6b967241cedbb810a0b6862d5aac9b08c
Jonwheeler <jon.wheeler7@gmail.com>
2012-08-19 10:18:28 -0500

    add-devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 LearnLive::Application.routes.draw do
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+
   root :to => 'pages#home'
-  match '/auth/facebook/callback', to: 'sessions#create', :as => :login
-  match '/auth/failure', to: 'pages#home'
-  match '/logout', to: 'sessions#destroy', :as => :logout
+  # match '/auth/facebook/callback', to: 'sessions#create', :as => :login
+  # match '/auth/failure', to: 'pages#home'
+  # match '/logout', to: 'sessions#destroy', :as => :logout
 end
