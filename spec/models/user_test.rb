c7e35f8551d9ce2d6b6f2d8896b14e4e83fc68c4
Jonwheeler <jon.wheeler7@gmail.com>
2012-08-23 22:58:17 -0500

    add rspec

--- spec/models/user_test.rb
+++ spec/models/user_test.rb
@@ -2,8 +2,17 @@ require 'spec_helper'
 
 describe User do
 
-  it "should create a new user with the correct info" do
+  it "should create a new user with the correct info"
 
-  end
+# ### email
 
+  it "should not work with a bad email"
+
+# ### passwords
+
+  it "password should not work with mismatching passwords"
+
+  it "password should not work with out a number"
+
+  it "password should not work with out a capital letter"
 end
